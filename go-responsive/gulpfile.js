fb1bffd6386d4455ce2e06a23c1dbff8dae338b0
ycx123123 <2530313434@qq.com>
2020-06-07 00:10:54 +0800

    教程

---
+++ go-responsive/gulpfile.js
@@ -0,0 +1,58 @@
+
+var gulp = require('gulp');
+var markdown = require('gulp-markdown');
+var wrap = require('gulp-wrap');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+
+
+gulp.task('build', function () {
+    return gulp.src('src/md/**/*.md')
+        .pipe(markdown())
+        .pipe(wrap({src: 'src/layout/default.html'}))
+        .pipe(gulp.dest('dist'));
+});
+
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('sass', function () {
+    return gulp.src('src/style/main.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        })) .on('error', handleError)
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('dist/style'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('cp-assets', function () {
+    return gulp.src('src/assets/**/*')
+        .pipe(gulp.dest('dist/'));
+});
+
+
+
+gulp.task('rebuild', ['build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass', 'cp-assets', 'build'], function() {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch(['src/md/**/*.md', 'src/layout/*.html'], ['rebuild']);
+    gulp.watch(['src/style/*'], ['sass']);
+});
+gulp.task('default', ['browser-sync','watch']);
